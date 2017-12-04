334051a73e4238e2f32816b0a5956a152800486f
Pedro Freitas <dryginlogic@gmail.com>
2017-12-04 12:32:12 +0000

    4.1 & 4.2

---
+++ 2017/p4.rb
@@ -0,0 +1,26 @@
+input = File.read('./2017/input4.txt')
+passphrases = input.split("\n")
+
+# part 1
+
+def valid?(passphrase)
+  words = passphrase.split
+  words.uniq.size == words.size
+end
+
+puts passphrases.select { |p| valid? p }.size
+
+# part 2
+
+def valid?(passphrase)
+  seen = {}
+  words = passphrase.split
+  words.each do |word|
+    sorted_word = word.chars.sort.join
+    return false if seen[sorted_word]
+    seen[sorted_word] = true
+  end
+  return true
+end
+
+puts passphrases.select { |p| valid? p }.size
